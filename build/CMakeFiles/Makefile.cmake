1f2080052084a9f7f50c18743856d81f9e5cefd4
albud187 <alfab@MSI.localdomain>
2024-03-03 11:37:13 -0500

    rename

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -54,5 +54,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/multi_3d.dir/DependInfo.cmake"
+  "CMakeFiles/math_viz.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/math_viz.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alfab/experiments/math_viz/src/main.cpp" "CMakeFiles/math_viz.dir/src/main.cpp.o" "gcc" "CMakeFiles/math_viz.dir/src/main.cpp.o.d"
+  "/home/alfab/experiments/math_viz/src/multi_utils/camera.cpp" "CMakeFiles/math_viz.dir/src/multi_utils/camera.cpp.o" "gcc" "CMakeFiles/math_viz.dir/src/multi_utils/camera.cpp.o.d"
+  "/home/alfab/experiments/math_viz/src/multi_utils/mesh.cpp" "CMakeFiles/math_viz.dir/src/multi_utils/mesh.cpp.o" "gcc" "CMakeFiles/math_viz.dir/src/multi_utils/mesh.cpp.o.d"
+  "/home/alfab/experiments/math_viz/src/multi_utils/shaders.cpp" "CMakeFiles/math_viz.dir/src/multi_utils/shaders.cpp.o" "gcc" "CMakeFiles/math_viz.dir/src/multi_utils/shaders.cpp.o.d"
+  "/home/alfab/experiments/math_viz/src/multi_utils/world_transform.cpp" "CMakeFiles/math_viz.dir/src/multi_utils/world_transform.cpp.o" "gcc" "CMakeFiles/math_viz.dir/src/multi_utils/world_transform.cpp.o.d"
+  "/home/alfab/experiments/math_viz/src/ogl_utils/ogldev_math_3d.cpp" "CMakeFiles/math_viz.dir/src/ogl_utils/ogldev_math_3d.cpp.o" "gcc" "CMakeFiles/math_viz.dir/src/ogl_utils/ogldev_math_3d.cpp.o.d"
+  "/home/alfab/experiments/math_viz/src/ogl_utils/ogldev_util.cpp" "CMakeFiles/math_viz.dir/src/ogl_utils/ogldev_util.cpp.o" "gcc" "CMakeFiles/math_viz.dir/src/ogl_utils/ogldev_util.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
